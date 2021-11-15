a1b27b7f0f204297862a0652875258a8e2c9a997
jooon <joonho0410@naver.com>
2021-11-15 19:56:25 +0900

    UI 조금 수정

--- app/src/main/java/com/example/myapplication/FrontPageActivity.java
+++ app/src/main/java/com/example/myapplication/FrontPageActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class FrontPageActivity extends AppCompatActivity {
 
     Button my_post, my_reply, free_board, market_board, my_dm;
@@ -22,6 +24,35 @@ public class FrontPageActivity extends AppCompatActivity {
         free_board = findViewById(R.id.free_board);
         market_board = findViewById(R.id.market_board);
         my_dm = findViewById(R.id.my_dm);
+
+        findViewById(R.id.main_page).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(),FrontPageActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.map).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(),MapActivity.class);
+                intent.putExtra("SearchText","");
+                intent.putExtra("SearchStartText","");
+                intent.putExtra("SearchDestText","");
+                intent.putExtra("sd",0);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.logout).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                FirebaseAuth.getInstance().signOut();
+                startActivity(intent);
+            }
+        });
 
         View.OnClickListener onClickListener = new View.OnClickListener(){
             @Override
